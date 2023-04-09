5e171e01d09a68f4ed9f0ef3436fc2f07058d50e
Thom Davis <Techthomdavis@gmail.com>
2023-04-08 22:26:47 -0500

    finally working

--- app/Farmer.php
+++ app/Farmer.php
@@ -1,11 +1,16 @@
 <?php
 
-namespace Moosura\Gardner;
+namespace Gardner;
 
 class Farmer {
     public function inspire(): string
     {
         return 'Agriculture is our wisest pursuit, because it will in the end contribute most to real wealth, good morals, and happiness. - Thomas Jefferson';
+    }
+
+    public function lobster(): string
+    {
+        return 'Yum124';
     }
 }
 
--- src/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-require_once 'vendor/autoload.php';
-
-use Moosura\Gardner\Farmer;
-
-$class = new Farmer();
